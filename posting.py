4bf42925389327b9ae269bf3161026a8e61039fb
something999 <wongny1@uci.edu>
2020-05-07 08:31:13 -0700

    Added a basic class for postings. No functions except for init.

---
+++ posting.py
@@ -0,0 +1,6 @@
+class Posting:
+    def __init__(self, id, ranking, fields, positions):
+        self.id = id
+        self.ranking = ranking
+        self.fields = fields
+        self.pointers = pointers
