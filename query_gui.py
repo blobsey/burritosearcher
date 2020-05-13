9553b400db54fe39a133b7f9ca029867120664cb
Nam <50193793+rizuki17@users.noreply.github.com>
2020-05-13 00:01:02 -0700

    adjusted input and access pickle directly

--- query_gui.py
+++ query_gui.py
@@ -1,6 +1,7 @@
-import re, os, glob
+import re, os, glob, pickle
 from nltk.stem import PorterStemmer
-from unpickler import unpickle
+#from unpickler import unpickle
+from lookup import lookup
 
 reg = re.compile(r"[a-zA-Z]+")
 ps = PorterStemmer()
@@ -8,16 +9,18 @@ ps = PorterStemmer()
 def run_query():
     while True:
         results = []
-        print("Input query search: ")
-        query = input()
+        query = input("Input query search: ")
         tokens = [ps.stem(word.lower()) for word in re.findall(reg, query)]
         for token in tokens:
-            unpickle(token[0])
+            #unpickle(token[0])
             #get info from dump and rank
-            with open("./unpickled/" + token[0] + ".txt", "r+") as dump:
+            with open("./index/" + token[0] + ".p", "rb") as dump:
                 #TODO: Ranking and insert into results
-                pass
-        println([i for i in results]])
+                temp = pickle.load(dump)
+                for line in temp:
+                    #prints word
+                    print(temp)
+        println([i for i in results])
 
         #Clear the unpickled files
         for file in glob.glob('./unpickled/*.txt'):
