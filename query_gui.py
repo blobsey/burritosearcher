56506e0623c3a7f2a261e9e8deb891fee7166496
Nanonaitor <vnanoc@hotmail.com>
2020-05-12 20:38:59 -0700

    query_gui.py progress
    More progress made for checkpoint M2

--- query_gui.py
+++ query_gui.py
@@ -1,24 +1,30 @@
-from lookup import lookup
-from unpickler import unpickle
+import re, os, glob
 from nltk.stem import PorterStemmer
+from unpickler import unpickle
 
 reg = re.compile(r"[a-zA-Z]+")
 ps = PorterStemmer()
 
 def run_query():
     while True:
+        results = []
         print("Input query search: ")
         query = input()
         tokens = [ps.stem(word.lower()) for word in re.findall(reg, query)]
         for token in tokens:
             unpickle(token[0])
-        #get info from dump
-        #rank
-        #sort on ranking
-        '''results = ["page1", "page2"]
-        println([i for i in results]])'''
-        #Find documents
-        #clear the dump file (has all unpickled information!)
+            #get info from dump and rank
+            with open("./unpickled/" + token[0] + ".txt", "r+") as dump:
+                #TODO: Ranking and insert into results
+                pass
+        println([i for i in results]])
+
+        #Clear the unpickled files
+        for file in glob.glob('./unpickled/*.txt'):
+            try:
+                file.unlink()
+            except OSError as e:
+                print("Error: %s : %s" % (file, e.strerror))
 
 # temp[token][docid].termfreq = integer 
 # temp[token][docid].tfidf = float 
