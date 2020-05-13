fb45a73bb9782c91e5e0751c0b22f19cbd191820
Nanonaitor <vnanoc@hotmail.com>
2020-05-12 19:32:57 -0700

    query_gui
    progress made

--- query_gui.py
+++ query_gui.py
@@ -1,18 +1,27 @@
 from lookup import lookup
 from unpickler import unpickle
+from nltk.stem import PorterStemmer
+
+reg = re.compile(r"[a-zA-Z]+")
+ps = PorterStemmer()
 
 def run_query():
     while True:
         print("Input query search: ")
         query = input()
-        #Tokenize + stem (also lowercase)
-        # token_amount = len(tokens)
-        # for token in query:
-        #     unpickle(token.charAt(0))
-
+        tokens = [ps.stem(word.lower()) for word in re.findall(reg, query)]
+        for token in tokens:
+            unpickle(token[0])
+        #get info from dump
+        #rank
+        #sort on ranking
+        '''results = ["page1", "page2"]
+        println([i for i in results]])'''
         #Find documents
         #clear the dump file (has all unpickled information!)
 
+# temp[token][docid].termfreq = integer 
+# temp[token][docid].tfidf = float 
 
 if __name__ == "__main__":
     run_query()
