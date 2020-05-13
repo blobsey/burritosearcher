39ce18377128af8fa5aea4809446da6fd03c1a65
Blake Randall <blrandal@uci.edu>
2020-05-12 17:04:07 -0700

    Update lookup.py

--- lookup.py
+++ lookup.py
@@ -1,9 +1,11 @@
 import pickle
 
+#helper function
+#takes a docid in, returns the filename
+#of the corresponding document
 def lookup(docid):
     with open("lookup.meme", "rb") as file:
         lookup = pickle.load(file)
     
-    filename = lookup[docid][1]
-    with open(filename, "rb") as fileList:
-        return fileList[docid][1]
+    return lookup[docid][1]
+    
